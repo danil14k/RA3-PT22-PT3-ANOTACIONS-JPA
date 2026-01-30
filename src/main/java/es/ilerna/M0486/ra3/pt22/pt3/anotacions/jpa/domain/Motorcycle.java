bd84eee496459b9c8159500af920162015d13b81
DanilDigger <danillitnevskiy14k@alumnes.ilerna.com>
2026-01-30 19:50:21 +0100

    Add Motorcycle entity extending Vehicle
    Introduces the Motorcycle JPA entity with a hasSidecar property, extending the Vehicle class and mapping to the 'motorcycle' table.

---
+++ src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/domain/Motorcycle.java
@@ -0,0 +1,22 @@
+package es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "motorcycle")
+@PrimaryKeyJoinColumn(name = "id")
+public class Motorcycle extends Vehicle {
+
+    private boolean hasSidecar;
+
+    public Motorcycle() {
+    }
+
+    public boolean isHasSidecar() {
+        return hasSidecar;
+    }
+
+    public void setHasSidecar(boolean hasSidecar) {
+        this.hasSidecar = hasSidecar;
+    }
+}
