2700687e302d1c45a30ba7de67b8daed6b483546
DanilDigger <danillitnevskiy14k@alumnes.ilerna.com>
2026-01-30 19:49:17 +0100

    Add JPA annotations and fields to Car entity
    Car now extends Vehicle and is annotated as a JPA entity with table and primary key join column. Added doors and seats fields with getters and setters.

--- src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/domain/Car.java
+++ src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/domain/Car.java
@@ -1,5 +1,31 @@
 package es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain;
 
-public class Car {
+import javax.persistence.*;
 
+@Entity
+@Table(name = "car")
+@PrimaryKeyJoinColumn(name = "id")
+public class Car extends Vehicle {
+
+    private Integer doors;
+    private Integer seats;
+
+    public Car() {
+    }
+
+    public Integer getDoors() {
+        return doors;
+    }
+
+    public void setDoors(Integer doors) {
+        this.doors = doors;
+    }
+
+    public Integer getSeats() {
+        return seats;
+    }
+
+    public void setSeats(Integer seats) {
+        this.seats = seats;
+    }
 }
