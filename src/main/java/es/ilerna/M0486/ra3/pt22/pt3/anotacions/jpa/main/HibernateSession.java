18da83bbc4da11fbe42d9782467934c281c2cf6e
DanilDigger <danillitnevskiy14k@alumnes.ilerna.com>
2026-01-30 20:20:52 +0100

    Fix package imports for annotated classes in HibernateSession
    Updated imports and class references in HibernateSession to use the correct package for domain classes. This change ensures that the annotated classes are properly registered with Hibernate, resolving issues caused by incorrect package paths.

--- src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/main/HibernateSession.java
+++ src/main/java/es/ilerna/M0486/ra3/pt22/pt3/anotacions/jpa/main/HibernateSession.java
@@ -1,39 +1,46 @@
 package es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.main;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+
+import es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain.Car;
+import es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain.Motorcycle;
+import es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain.Person;
+import es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain.Plane;
+import es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain.Student;
+import es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain.Teacher;
+import es.ilerna.M0486.ra3.pt22.pt3.anotacions.jpa.domain.Vehicle;
 
 public class HibernateSession {
 
 	private static final SessionFactory sessionFactory = buildSessionFactory();
-	
+
 	private static SessionFactory buildSessionFactory() {
 		try {
 			Configuration configuration = new Configuration();
-			
-			configuration.configure(); 
-			configuration.addAnnotatedClass(es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain.Person.class);
-			configuration.addAnnotatedClass(es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain.Student.class);
-			configuration.addAnnotatedClass(es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain.Teacher.class);
 
-			configuration.addAnnotatedClass(es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain.Vehicle.class);
-			configuration.addAnnotatedClass(es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain.Car.class);
-			configuration.addAnnotatedClass(es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain.Plane.class);
-			configuration.addAnnotatedClass(es.ilerna.M0486.ra3.pt22.anotacions.jpa.domain.Motorcycle.class);
-			
+			configuration.configure();
+
+			configuration.addAnnotatedClass(Person.class);
+			configuration.addAnnotatedClass(Student.class);
+			configuration.addAnnotatedClass(Teacher.class);
+			configuration.addAnnotatedClass(Vehicle.class);
+			configuration.addAnnotatedClass(Car.class);
+			configuration.addAnnotatedClass(Plane.class);
+			configuration.addAnnotatedClass(Motorcycle.class);
+
 			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-				.applySettings(configuration.getProperties())
-				.build();
-			
-			return configuration.buildSessionFactory(serviceRegistry);		
+					.applySettings(configuration.getProperties()).build();
+
+			return configuration.buildSessionFactory(serviceRegistry);
 		} catch (Throwable ex) {
 			System.err.println("Error creando SessionFactory: " + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
 	}
-	
+
 	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
